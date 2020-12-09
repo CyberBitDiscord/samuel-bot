e5100c92cfb1552241c793d2594fccf39aaacdae
Gabriel Eich <eich.gabriel1@gmail.com>
2020-12-09 20:49:46 -0300

    log errors in channel

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const { execute } = require('./commands')
 const { token } = require('../bot.config.json')
-const { prefix } = require('../config.json')
+const { prefix } = require('../config.json');
+const { sendErrorToChannel } = require('./utils/errorsLogs');
 const client = new Discord.Client();
 //Mensagem quando um boot entrar no servidor
 client.on('ready', () => {
@@ -10,30 +11,34 @@ client.on('ready', () => {
 
 //Quando for digitado no chat 
 client.on('message', async msg => {
-  if (msg.content.startsWith(prefix)) {
-    const command = msg.content.split(' ')[1]
-    const commandReturn = execute({ command, msg })
-  
-    if(!commandReturn) {
-      msg.channel.send(`O comando ${command} não existe!`)
-    } else if(commandReturn === 401) {
-      msg.channel.send(`Você não tem permissão para usar esse comando!`)
-    }
-
-  } else if(msg.reference) {
-    const msgRef = await msg.channel.messages.fetch(msg.reference.messageID)
-
-    if(msgRef.embeds[0]) {
-      const command = msgRef.embeds[0].title
-
-      const commandReturn = execute({ command, msg, msgRef})
+  try {
+    if (msg.content.startsWith(prefix)) {
+      const command = msg.content.split(' ')[1]
+      const commandReturn = execute({ command, msg })
     
       if(!commandReturn) {
-        return
-      } else if(commandReturn === 403) {
+        msg.channel.send(`O comando ${command} não existe!`)
+      } else if(commandReturn === 401) {
         msg.channel.send(`Você não tem permissão para usar esse comando!`)
       }
+
+    } else if(msg.reference) {
+      const msgRef = await msg.channel.messages.fetch(msg.reference.messageID)
+
+      if(msgRef.embeds[0]) {
+        const command = msgRef.embeds[0].title
+
+        const commandReturn = execute({ command, msg, msgRef})
+      
+        if(!commandReturn) {
+          return
+        } else if(commandReturn === 403) {
+          msg.channel.send(`Você não tem permissão para usar esse comando!`)
+        }
+      }
     }
+  } catch(err) {
+    sendErrorToChannel(msg, err)
   }
 
   return
---
+++ src/utils/errorsLogs.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js')
+
+exports.sendErrorToChannel = (msg, err) => {
+    const channels = msg.guild.channels.cache.array()
+    const errorChannel = channels.find(c => c.name === 'erros-samuel')
+
+    if(!errorChannel) return
+
+    const embed = new Discord.MessageEmbed()
+    .setColor("#DC143C")
+    .setTitle(err.name)
+    .setDescription(err.stack)
+
+    errorChannel.send(embed)
+}
