863e2fba0ccb2b0c154d001f7fdf7545ddb4a5dd
Gabriel Eich <eich.gabriel1@gmail.com>
2020-12-09 20:52:33 -0300

    forced error for test removed

--- src/commands/help.js
+++ src/commands/help.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 
 const help = ({ commands, msg }) => {
     const commandsList = Object.keys(commands).map(c => `${commands[c].description} \n`)
-dddd
     embed = new Discord.MessageEmbed()
       .setColor("#5c0fd9")
       .setTitle("Help")
